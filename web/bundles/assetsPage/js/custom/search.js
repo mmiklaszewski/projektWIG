795da2d67059bb7594ecb94e4ca74c6a1a6850c6
mmiklaszewski <miklaszewski.maciek93@gmail.com>
2016-01-15 11:44:40 +0100

    change search js

--- web/bundles/assetsPage/js/custom/search.js
+++ web/bundles/assetsPage/js/custom/search.js
@@ -1,51 +1,47 @@
 $().ready(function() {
 
     $(document).on("keyup", "#search_form",  function(e) {
-    //$('#search_form').on('keyup blur', (function (e) {
-    //     $("#search_form").submit(function (e) {
         e.preventDefault();
         var $url = $(this).attr('action');
         var $data = $(this).serialize();
+        var $length = ($(this).find('input[name="fraza"]').val()).length;
 
+        if($length>=3) {
 
+            $.ajax({
+                type: "POST",
+                cache: false,
+                url: $url,
+                data: $data,
+                success: function (result) {
 
-
-        $.ajax({
-            type: "POST",
-            cache: false,
-            url: $url,
-            data: $data,
-            success: function (result) {
-                   console.log(result);
 //                   $("#html").html(result);
-                var $container = $(result).find('#main').html();
+                    var $container = $(result).find('#main').html();
 //                    var $container = $(result).find('#main').html();  //do pelnej zamiany na strona główna
 
 
+                    //$login = '<div id="alert" class="alert alert-success alert-dismissable">Pomyślnie zalogowano</div>';
+                    //
+                    //$(".nav").html($menu);
+                    //$(".panel-title").text('Zalogowano');
+                    //$(".panel-body").html($login);
+                    //sessionStorage.counter = 0;
+                    //
+                    //if (typeof(Storage) !== "undefined") {
+                    //    localStorage.setItem("username", $username);
+                    //    //    $('#username').val(localStorage.getItem("username"));
+                    //    //    //console.log(localStorage.getItem("username"));
+                    //}
+
+                    $("#main").html($container);  //do pelnej zamiany na strona główna
 
 
-                //$login = '<div id="alert" class="alert alert-success alert-dismissable">Pomyślnie zalogowano</div>';
-                //
-                //$(".nav").html($menu);
-                //$(".panel-title").text('Zalogowano');
-                //$(".panel-body").html($login);
-                //sessionStorage.counter = 0;
-                //
-                //if (typeof(Storage) !== "undefined") {
-                //    localStorage.setItem("username", $username);
-                //    //    $('#username').val(localStorage.getItem("username"));
-                //    //    //console.log(localStorage.getItem("username"));
-                //}
-
-                $("#main").html($container);  //do pelnej zamiany na strona główna
-
-
-
-            }
-        });
-    })
+                }
+            });
+        }
+    });
     //)
-    ;
+
 
     //$('#login_form input').on('keyup blur', function () { // fires on every keyup & blur
     //    if ($('#login_form').valid()) {                   // checks form for validity
